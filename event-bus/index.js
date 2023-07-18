8b01ae36d9d46238924a9af4db74143a7b860824
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-18 22:38:58 +0700

    commit

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,8 +8,18 @@ const events = [];
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/events", async (req, res) => {
-    res.send(events)
-})
+app.get("/events", (req, res) => {
+    res.send(events);
+});
+
+app.post("events", (req, res) => {
+    const event = req.body;
+    events.push(event);
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch((e) => console.log(e));
+    axios.post('http://comments-clusterip-srv:4001/events', event).catch((e) => console.log(e));
+    axios.post('http://query-clusterip-srv:4002/events', event).catch((e) => console.log(e));
+    axios.post('http://moderation-clusterip-srv:4003/events', event).catch((e) => console.log(e));
+
+});
 
 app.listen(4005, () => console.log('Event bus is listening on port 4005'));
