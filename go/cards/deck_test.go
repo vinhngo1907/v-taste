ca427d0af4d50ad6396092d2d767d745e339adc7
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-18 17:42:15 +0700

    chu ba bi bo nha nho

---
+++ go/cards/deck_test.go
@@ -0,0 +1,16 @@
+package main
+
+// import ( "os", "testing" )
+
+// func TestNewDeck(t *testing.T) {
+// 	d := newDeck()
+// 	if len(d) != 52 {
+// 		t.Error
+// 	}
+// }
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Coders Tokyo")
+}
