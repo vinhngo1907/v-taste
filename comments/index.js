0e32bcb7c9663f86c80a907ac070b1d7341d41ac
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-18 12:15:13 +0700

    commit

--- comments/index.js
+++ comments/index.js
@@ -14,4 +14,16 @@ app.get('/posts/:id/comments', async (req, res) => {
     res.send(commentByPostId[req.params.id] || []);
 });
 
+app.post('/post/:id/comments', async (req, res) => {
+    const id = randomBytes(4).toString('hex');
+    const { content } = req.body;
+    const comments = commentByPostId[req.params.id] || [];
+    comments.push({ id, content, status: 'pending' });
+
+    await axios.post('http://event-bus-srv:4005/events', {
+        type: 'CommentCreate',
+        data: { id, content, postId: req.params.id, status: 'pending' }
+    })
+});
+
 app.listen(4001, () => console.log("Comments on port 4001"));
