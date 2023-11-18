9f71e8066245469ca77324996aeb01c0c997770e
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-18 18:07:08 +0700

    LICENSE

---
+++ posts/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(cors);
+
+app.listen(4000, () => {
+    console.log("Server started 4000");
+});
