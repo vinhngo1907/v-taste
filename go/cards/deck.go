bc070c876d64caac008b24696a9515a049eeb366
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-11-18 18:05:06 +0700

    commit

--- go/cards/deck.go
+++ go/cards/deck.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+type deck []string
+
+func newDeck() deck {
+	deck := deck{}
+	cardSuits := []string{"diamons", "hearts", "spades", "clubs"}
+	cardValues := []string{"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "Ace", "King"}
+	for _, cardSuit := range cardSuits {
+		for _, cardValue := range cardValues {
+			deck = append(deck, cardValue+ " of "+cardSuit)
+		}
+	}
+
+	return deck
+}
+
+func (d deck) print () {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
